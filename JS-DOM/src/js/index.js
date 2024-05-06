f032e1608c1b6230b89b83857293feed2ab4b8be
asmat1k <dobrovti@mail.ru>
2024-05-06 20:28:30 +0300

    init: перенес проект на сборщик, для разделения логики

---
+++ JS-DOM/src/js/index.js
@@ -0,0 +1,6 @@
+import '../css/style.css';
+
+const appRoot = document.getElementById('app-root');
+console.log(appRoot);
+
+console.log(externalService.getAllCountries());
--- app.js
+++
@@ -1,3 +0,0 @@
-const appRoot = document.getElementById('app-root'); // add all your elements into this element
-// insert your code here
-// use externalService to get necessary data
