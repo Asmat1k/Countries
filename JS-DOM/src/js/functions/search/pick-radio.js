c19fc601dd9b19e37cfae333b4c21482d7525651
asmat1k <dobrovti@mail.ru>
2024-05-13 20:45:16 +0300

    feat: добавил рендер дефолтной таблицы при клике на не выбранную кнопку

--- JS-DOM/src/js/functions/search/pick-radio.js
+++ JS-DOM/src/js/functions/search/pick-radio.js
@@ -1,23 +1,33 @@
+import { refreshTable } from '../table/refresh-table';
 import { refreshSelect } from './refresh-select';
 
 export function pickRadioBtn() {
   const RADIOS = document.querySelectorAll('input[type="radio"]');
+  let lastActiveBtnId = '';
 
   RADIOS.forEach((item) => {
     item.addEventListener('click', () => {
-      switch (item.id) {
-        case 'region': {
-          refreshSelect(externalService.getRegionsList(), item.id);
-          break;
+      if (lastActiveBtnId !== item.id) {
+        // Для избежания перерисовки при клике на первом рендере
+        if (lastActiveBtnId) {
+          refreshTable(externalService.getAllCountries());
         }
-        case 'language': {
-          refreshSelect(externalService.getLanguagesList(), item.id);
-          break;
-        }
-        default: {
-          break;
+
+        switch (item.id) {
+          case 'region': {
+            refreshSelect(externalService.getRegionsList(), item.id);
+            break;
+          }
+          case 'language': {
+            refreshSelect(externalService.getLanguagesList(), item.id);
+            break;
+          }
+          default: {
+            break;
+          }
         }
       }
+      lastActiveBtnId = item.id;
     });
   });
 }
